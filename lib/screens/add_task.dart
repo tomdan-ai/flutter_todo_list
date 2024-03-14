6f3b538bcab570d341adf2c4b4406fdd56d5b9dc
tomdan-ai <tomudoh258@gmail.com>
2024-03-14 01:31:37 +0100

    Create and design the add task scrren

---
+++ lib/screens/add_task.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/material.dart';
+
+class AddTaskScreen extends StatefulWidget {
+  @override
+  _AddTaskScreenState createState() => _AddTaskScreenState();
+}
+
+class _AddTaskScreenState extends State<AddTaskScreen> {
+  final TextEditingController _titleController = TextEditingController();
+  final TextEditingController _detailController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Add Task',
+          style: TextStyle(color: Colors.white),
+        ),
+        backgroundColor: Color(0xFF9395D3),
+        iconTheme: IconThemeData(color: Colors.white), // Set the color of the return arrow to white
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            TextField(
+              controller: _titleController,
+              decoration: InputDecoration(
+                hintText: 'Title',
+              ),
+            ),
+            SizedBox(height: 16.0),
+            TextField(
+              controller: _detailController,
+              decoration: InputDecoration(
+                hintText: 'Detail',
+              ),
+            ),
+            SizedBox(height: 32.0),
+            ElevatedButton(
+              onPressed: () {
+                final title = _titleController.text;
+                final detail = _detailController.text;
+                if (title.isNotEmpty && detail.isNotEmpty) {
+                  Navigator.pop(context, {
+                    'title': title,
+                    'detail': detail,
+                  });
+                }
+              },
+              child: Text(
+                'ADD',
+                style: TextStyle(
+                  color: Colors.white,
+                ),
+              ),
+              style: ElevatedButton.styleFrom(
+                // primary: Colors.purple,
+                padding: EdgeInsets.symmetric(horizontal: 32.0, vertical: 16.0),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    _titleController.dispose();
+    _detailController.dispose();
+    super.dispose();
+  }
+}
