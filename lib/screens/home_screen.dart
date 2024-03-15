225c1266c9ff7c5364639fd933294025711f541e
tomdan-ai <tomudoh258@gmail.com>
2024-03-15 08:21:30 +0100

    Implement all best practice

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -5,7 +5,9 @@ import 'package:flutter_todo_list/screens/completed_task.dart'; // Importing the
 import 'package:shared_preferences/shared_preferences.dart'; // Importing shared preferences for data persistence
 
 class TodoList extends StatefulWidget { // Define a StatefulWidget for the todo list
+ const TodoList({Key? key}) : super(key:key);
   @override
+  // ignore: library_private_types_in_public_api
   _TodoListState createState() => _TodoListState(); // Create state for the TodoList
 }
 
@@ -63,13 +65,13 @@ class _TodoListState extends State<TodoList> { // Define state for the todo list
           ? AppBar( // AppBar for the top navigation bar
               title: Row( // Row widget to arrange children widgets horizontally
                 children: [
-                  Text( // Text widget to display the title
+                  const Text( // Text widget to display the title
                     'TODO APP', // Title text
                     style: TextStyle(color: Colors.white), // Style the text color to white
                   ),
-                  Spacer(), // Spacer widget to fill remaining space
+                 const Spacer(), // Spacer widget to fill remaining space
                   IconButton( // IconButton widget for calendar icon
-                    icon: Icon(Icons.calendar_today), // Calendar icon
+                    icon: const Icon(Icons.calendar_today), // Calendar icon
                     color: Colors.white, // Set icon color to white
                     onPressed: () {
                       // Implement calendar icon functionality here
@@ -77,12 +79,12 @@ class _TodoListState extends State<TodoList> { // Define state for the todo list
                   ),
                 ],
               ),
-              backgroundColor: Color(0xFF9395D3), // Set the background color of the app bar
+              backgroundColor: const  Color(0xFF9395D3), // Set the background color of the app bar
             )
           : null, // Set app bar to null if not on the main screen
       body: _currentIndex == 0 // Show todo list on the main screen
           ? Padding( // Padding widget to add padding around the content
-              padding: EdgeInsets.symmetric(horizontal: 16.0), // Set padding to 16 pixels on horizontal axis
+              padding: const EdgeInsets.symmetric(horizontal: 16.0), // Set padding to 16 pixels on horizontal axis
               child: Column( // Column widget to arrange children widgets vertically
                 children: [
                   Expanded( // Expanded widget to fill remaining space
@@ -90,20 +92,20 @@ class _TodoListState extends State<TodoList> { // Define state for the todo list
                       itemCount: _todos.length, // Set the number of items in the list to the length of todo tasks
                       itemBuilder: (context, index) { // Item builder function to build each item in the list
                         return Padding( // Padding widget to add padding around the list item
-                          padding: EdgeInsets.only(bottom: 12.0), // Set padding to 12 pixels at the bottom
+                          padding: const EdgeInsets.only(bottom: 12.0), // Set padding to 12 pixels at the bottom
                           child: Card( // Card widget to display each task
-                            margin: EdgeInsets.only(top: 12.0), // Set margin to 12 pixels at the top
+                            margin: const EdgeInsets.only(top: 12.0), // Set margin to 12 pixels at the top
                             child: ListTile( // ListTile widget to display each task as a list item
                               title: Text( // Text widget to display the task title
                                 _todos[index].split('\n')[0], // Get the task title from the todo list
-                                style: TextStyle( // Style the text
+                                style: const TextStyle( // Style the text
                                   fontSize: 18, // Set font size to 18
                                   color: Color(0xFF9395D3), // Set text color to purple
                                 ),
                               ),
                               subtitle: Text( // Text widget to display the task detail
                                 _todos[index].split('\n')[1], // Get the task detail from the todo list
-                                style: TextStyle( // Style the text
+                                style: const TextStyle( // Style the text
                                   color: Colors.black, // Set text color to black
                                 ),
                               ),
@@ -111,8 +113,8 @@ class _TodoListState extends State<TodoList> { // Define state for the todo list
                                 mainAxisSize: MainAxisSize.min, // Set the main axis size to minimum
                                 children: [
                                   IconButton( // IconButton widget for edit icon
-                                    icon: Icon(Icons.create_outlined), // Edit icon
-                                    color: Color(0xFF9395D3), // Set icon color to purple
+                                    icon: const Icon(Icons.create_outlined), // Edit icon
+                                    color: const Color(0xFF9395D3), // Set icon color to purple
                                     onPressed: () async { // Callback function called when the button is pressed
                                       final result = await Navigator.push( // Navigate to the edit task screen
                                         context,
@@ -132,15 +134,15 @@ class _TodoListState extends State<TodoList> { // Define state for the todo list
                                     },
                                   ),
                                   IconButton( // IconButton widget for delete icon
-                                    icon: Icon(Icons.delete_outline_rounded), // Delete icon
-                                    color: Color(0xFF9395D3), // Set icon color to purple
+                                    icon: const Icon(Icons.delete_outline_rounded), // Delete icon
+                                    color: const Color(0xFF9395D3), // Set icon color to purple
                                     onPressed: () { // Callback function called when the button is pressed
                                       _deleteTask(index); // Delete the task
                                     },
                                   ),
                                   IconButton( // IconButton widget for complete icon
-                                    icon: Icon(Icons.check_circle_outline), // Complete icon
-                                    color: Color(0xFF9395D3), // Set icon color to purple
+                                    icon: const Icon(Icons.check_circle_outline), // Complete icon
+                                    color: const Color(0xFF9395D3), // Set icon color to purple
                                     onPressed: () { // Callback function called when the button is pressed
                                       _completeTask(index); // Mark task as completed
                                     },
@@ -160,9 +162,9 @@ class _TodoListState extends State<TodoList> { // Define state for the todo list
       floatingActionButton: _currentIndex == 0 // Show floating action button only on the main screen
           ? ClipOval( // ClipOval widget to clip the child widget in oval shape
               child: Material( // Material widget for elevation and ink effects
-                color: Color(0xFF9395D3), // Set background color to purple
+                color: const Color(0xFF9395D3), // Set background color to purple
                 child: InkWell( // InkWell widget for tap animation
-                  child: SizedBox( // SizedBox widget for fixed size
+                  child: const SizedBox( // SizedBox widget for fixed size
                     width: 56, // Set width to 56 pixels
                     height: 56, // Set height to 56 pixels
                     child: Icon(Icons.add, color: Colors.white), // Icon widget for add icon with white color
@@ -190,9 +192,9 @@ class _TodoListState extends State<TodoList> { // Define state for the todo list
             _currentIndex = index; // Set current index to the tapped index
           });
         },
-        items: [ // List of BottomNavigationBarItem widgets
+        items: const [ // List of BottomNavigationBarItem widgets
           BottomNavigationBarItem( // BottomNavigationBarItem for all tasks
-            icon: Icon(Icons.list), // Icon for all tasks
+            icon:  Icon(Icons.list), // Icon for all tasks
             label: 'All', // Label for all tasks
           ),
           BottomNavigationBarItem( // BottomNavigationBarItem for completed tasks
