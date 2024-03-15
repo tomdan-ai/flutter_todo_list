4c6d8897d9d19166855e17d1e5460d8538b3e3f4
tomdan-ai <tomudoh258@gmail.com>
2024-03-15 08:22:12 +0100

    Add consturctor key and const keyword

--- lib/screens/add_task.dart
+++ lib/screens/add_task.dart
@@ -1,6 +1,10 @@
+// ignore_for_file: library_private_types_in_public_api
+
 import 'package:flutter/material.dart'; // Importing the material package for Flutter UI components
 
-class AddTaskScreen extends StatefulWidget { // Define a StatefulWidget for the add task screen
+class AddTaskScreen extends StatefulWidget {
+  const AddTaskScreen({Key? key}) : super(key: key);
+ // Define a StatefulWidget for the add task screen
   @override
   _AddTaskScreenState createState() => _AddTaskScreenState(); // Create state for the AddTaskScreen
 }
@@ -13,31 +17,31 @@ class _AddTaskScreenState extends State<AddTaskScreen> { // Define state for the
   Widget build(BuildContext context) { // Build method to create the UI for the add task screen
     return Scaffold( // Scaffold widget provides a basic structure for the screen
       appBar: AppBar( // AppBar for the top navigation bar
-        title: Text( // Text widget to display the title
+        title: const Text( // Text widget to display the title
           'Add Task', // Title text
           style: TextStyle(color: Colors.white), // Style the text color to white
         ),
-        backgroundColor: Color(0xFF9395D3), // Set the background color of the app bar
-        iconTheme: IconThemeData(color: Colors.white), // Set arrow color to white
+        backgroundColor: const Color(0xFF9395D3), // Set the background color of the app bar
+        iconTheme: const IconThemeData(color: Colors.white), // Set arrow color to white
       ),
       body: Padding( // Padding widget to add padding around the content
-        padding: EdgeInsets.all(16.0), // Set padding to 16 pixels on all sides
+        padding: const EdgeInsets.all(16.0), // Set padding to 16 pixels on all sides
         child: Column( // Column widget to arrange children widgets vertically
           children: [ // List of children widgets
             TextField( // Text field widget for entering the task title
               controller: _titleController, // Set the controller to manage the text field
-              decoration: InputDecoration( // Input decoration to customize the appearance of the text field
+              decoration: const InputDecoration( // Input decoration to customize the appearance of the text field
                 hintText: 'Title', // Hint text to display when the field is empty
               ),
             ),
-            SizedBox(height: 16.0), // SizedBox widget for adding vertical space
+            const SizedBox(height: 16.0), // SizedBox widget for adding vertical space
             TextField( // Text field widget for entering the task detail
               controller: _detailController, // Set the controller to manage the text field
-              decoration: InputDecoration( // Input decoration to customize the appearance of the text field
+              decoration: const InputDecoration( // Input decoration to customize the appearance of the text field
                 hintText: 'Detail', // Hint text to display when the field is empty
               ),
             ),
-            SizedBox(height: 32.0), // SizedBox widget for adding vertical space
+            const SizedBox(height: 32.0), // SizedBox widget for adding vertical space
             ElevatedButton( // Elevated button widget for adding the task
               onPressed: () { // Callback function called when the button is pressed
                 final title = _titleController.text; // Get the title from the text field
@@ -49,15 +53,15 @@ class _AddTaskScreenState extends State<AddTaskScreen> { // Define state for the
                   });
                 }
               },
-              child: Text( // Text widget for the button label
+              style: ElevatedButton.styleFrom( // Style the elevated button
+                backgroundColor: const Color(0xFF9395D3), // Set button color to the desired color
+                padding: const EdgeInsets.symmetric(horizontal: 32.0, vertical: 16.0), // Set padding for the button
+              ),
+              child: const Text( // Text widget for the button label
                 'ADD', // Button label text
                 style: TextStyle( // Style the button label text
                   color: Colors.white, // Set text color to white
                 ),
-              ),
-              style: ElevatedButton.styleFrom( // Style the elevated button
-                backgroundColor: Color(0xFF9395D3), // Set button color to the desired color
-                padding: EdgeInsets.symmetric(horizontal: 32.0, vertical: 16.0), // Set padding for the button
               ),
             ),
           ],
