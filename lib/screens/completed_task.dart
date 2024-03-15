0cc896db4569c05044665bd59b209f801035fc36
tomdan-ai <tomudoh258@gmail.com>
2024-03-15 08:23:34 +0100

    Add const keyword and ignore private type in public api

--- lib/screens/completed_task.dart
+++ lib/screens/completed_task.dart
@@ -3,9 +3,10 @@ import 'package:flutter/material.dart'; // Importing the material package for Fl
 class CompletedTasksScreen extends StatefulWidget { // Define a StatefulWidget for the completed tasks screen
   final List<String> completedTasks; // Define a list to hold completed tasks
 
-  CompletedTasksScreen({required this.completedTasks}); // Constructor to initialize the completed tasks list
+  const CompletedTasksScreen({Key? key, required this.completedTasks}) : super(key: key); // Constructor to initialize the completed tasks list
 
   @override
+  // ignore: library_private_types_in_public_api
   _CompletedTasksScreenState createState() => _CompletedTasksScreenState(); // Create state for the CompletedTasksScreen
 }
 
@@ -14,12 +15,12 @@ class _CompletedTasksScreenState extends State<CompletedTasksScreen> { // Define
   Widget build(BuildContext context) { // Build method to create the UI for the completed tasks screen
     return Scaffold( // Scaffold widget provides a basic structure for the screen
       appBar: AppBar( // AppBar for the top navigation bar
-        title: Text( // Text widget to display the title
+        title: const Text( // Text widget to display the title
           'Completed Tasks', // Title text
           style: TextStyle(color: Colors.white), // Style the text color to white
         ),
-        backgroundColor: Color(0xFF9395D3), // Set the background color of the app bar
-        iconTheme: IconThemeData(color: Colors.white), // Set the color of the return arrow to white
+        backgroundColor: const Color(0xFF9395D3), // Set the background color of the app bar
+        iconTheme: const IconThemeData(color: Colors.white), // Set the color of the return arrow to white
       ),
       body: ListView.builder( // ListView builder to create a scrollable list of completed tasks
         itemCount: widget.completedTasks.length, // Set the number of items in the list to the length of the completed tasks list
@@ -28,7 +29,7 @@ class _CompletedTasksScreenState extends State<CompletedTasksScreen> { // Define
             child: ListTile( // ListTile widget to display each task as a list item
               title: Text( // Text widget to display the task name
                 widget.completedTasks[index], // Get the task name from the completed tasks list
-                style: TextStyle(fontSize: 18), // Style the text size
+                style: const TextStyle(fontSize: 18), // Style the text size
               ),
             ),
           );
